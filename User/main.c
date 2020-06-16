7ae7f49fabb5344879386d95724fda129d1563fa
Najibkassab <najeebkassab@hotmail.com>
2020-06-16 15:26:11 +0300

    cleaning up and changing name from MX_ADC_Init() to ADC_Init()

--- User/main.c
+++ User/main.c
@@ -39,11 +39,12 @@
 /* Includes ------------------------------------------------------------------*/
 #include "BOS.h"
 
+GPIO_InitTypeDef mypin;
 
 /* Private variables ---------------------------------------------------------*/
-uint32_t in;
+uint8_t in[8];
 /* Private function prototypes -----------------------------------------------*/
-
+void GPIO_config(void);
 /* Main functions ------------------------------------------------------------*/
 
 int main(void)
@@ -85,15 +86,24 @@ int main(void)
 /* User Task */
 void UserTask(void * argument)
 {
+
+//	GPIO_config();
 	HAL_ADCEx_Calibration_Start(&hadc);
-  HAL_ADC_Start_DMA(&hadc,&in,1);
+  HAL_ADC_Start_DMA(&hadc,(uint32_t *)in,8);
   /* Infinite loop */
   for(;;)
   {
 
 	}
 }
-
+void GPIO_config(void)
+{
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	mypin.Pin=GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4;
+	mypin.Mode=GPIO_MODE_ANALOG;
+	mypin.Pull= GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA,&mypin);
+}
 /*-----------------------------------------------------------*/
 
 /************************ (C) COPYRIGHT HEXABITZ *****END OF FILE****/
