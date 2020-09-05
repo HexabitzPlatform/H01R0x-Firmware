1a879bfabf68a58fea074da2df3ca671a82d8535
Najibkassab <najeebkassab@hotmail.com>
2020-09-05 16:30:34 +0300

    update -->0.2.3

--- H01R0/H01R0_gpio.c
+++ H01R0/H01R0_gpio.c
@@ -33,7 +33,7 @@
   */
 
 /*
-		MODIFIED by Hexabitz for BitzOS (BOS) V0.2.1 - Copyright (C) 20167 Hexabitz
+		MODIFIED by Hexabitz for BitzOS (BOS) V0.2.3 - Copyright (C) 20167 Hexabitz
     All rights reserved
 */
 
