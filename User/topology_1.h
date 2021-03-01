562326a3fd18d56bab2a6b031456632f0b192f82
Najibkassab <najeebkassab@hotmail.com>
2021-03-01 14:07:45 +0200

    -Remove Keil project file and Folder and delete .hex files from Compiled folder -Porting H01R0x & P01R0x Module to Stm32CubeIDE

--- User/topology_1.h
+++ User/topology_1.h
@@ -1,5 +1,5 @@
 /*
-    BitzOS (BOS) V0.0.0 - Copyright (C) 2016 Hexabitz
+    BitzOS (BOS) V0.2.4 - Copyright (C) 2016 Hexabitz
     All rights reserved
 
     File Name     : topology_1.h
@@ -15,19 +15,18 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f0xx_hal.h"
-	 
 
-#define __N	2				// Number of array modules
+
+#define __N	2					// Number of array modules
 
 // Array modules
 #define _mod1	1<<3
 #define _mod2	2<<3
 
-
 // Topology
 static uint16_t array[__N][7] = {
-{ _H01R0, 0, 0, _mod2|P3, 0, 0, 0},									// Module 1
-{ _H01R0, 0, 0, _mod1|P3, 0, 0, 0},									// Module 2
+{ _H01R0, 0, 0,0, 0, 0,  _mod2|P5},							// Module 1
+{ _H01R0, 0, 0, 0, 0, _mod1|P5, 0},			        // Module 2
 };
 
 // Configurations for duplex serial ports
@@ -36,18 +35,21 @@ static uint16_t array[__N][7] = {
 	#define	_P1pol_normal	1
 	#define	_P2pol_normal	1
 	#define	_P3pol_normal	1
-	#define	_P4pol_normal	1	
+	#define	_P4pol_normal	1
 	#define	_P5pol_normal	1
-	#define	_P6pol_normal	1
+	#define	_P6pol_reversed	1
+
 #endif
+
 #if ( _module == 2 )
 	#define	H01R0	1
 	#define	_P1pol_normal	1
 	#define	_P2pol_normal	1
-	#define	_P3pol_reversed	1
-	#define	_P4pol_normal	1	
+	#define	_P3pol_normal	1
+	#define	_P4pol_normal	1
 	#define	_P5pol_normal	1
 	#define	_P6pol_normal	1
+
 #endif
 
 #ifdef __cplusplus
