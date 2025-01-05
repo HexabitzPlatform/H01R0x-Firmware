58f2d52d561f6abb4f92a25e81c9d672b2a1e177
ADEL\adool <adoolfaki@gmail.com>
2025-01-05 15:40:54 +0300

    Change BOS and Thirdparty submodules Commits

--- User/main.c
+++ User/main.c
@@ -28,10 +28,76 @@ int main(void) {
 /* User Task */
 void UserTask(void *argument) {
 
+	AddPortButton(MOMENTARY_NO, 2);   //Define a button connected to port P1
+	SetButtonEvents(2, 1, 0, 3, 0, 0, 0, 0, 0,1);    // Activate a click event and a pressed_for_x event for 3 seconds
+
+	RGB_LED_sweep(RGB_SWEEP_BASIC, 2000, -1);
+
 	// put your code here, to run repeatedly.
 	while (1) {
+
+
 
 	}
 }
 
+void buttonClickedCallback(uint8_t port){
+
+	SendMessageToModule(2,CODE_PING, 0);
+//	Delay_ms(20);
+
+	SendMessageToModule(4,CODE_PING, 0);
+//	Delay_ms(20);
+
+	SendMessageToModule(7,CODE_PING, 0);
+//	Delay_ms(20);
+
+	SendMessageToModule(8,CODE_PING, 0);
+//	Delay_ms(20);
+
+	SendMessageToModule(9,CODE_PING, 0);
+//	Delay_ms(20);
+
+	SendMessageToModule(10,CODE_PING, 0);
+//	Delay_ms(20);
+
+	Delay_ms(200);
+
+	SendMessageToModule(2,CODE_PING, 0);
+//	Delay_ms(20);
+
+	SendMessageToModule(4,CODE_PING, 0);
+//	Delay_ms(20);
+
+	SendMessageToModule(7,CODE_PING, 0);
+//	Delay_ms(20);
+
+	SendMessageToModule(8,CODE_PING, 0);
+//	Delay_ms(20);
+
+	SendMessageToModule(9,CODE_PING, 0);
+//	Delay_ms(20);
+
+	SendMessageToModule(10,CODE_PING, 0);
+//	Delay_ms(20);
+
+//	SendMessageToModule(2,CODE_PING, 0);
+//	Delay_ms(20);
+//
+//	SendMessageToModule(4,CODE_PING, 0);
+//	Delay_ms(20);
+//
+//	SendMessageToModule(7,CODE_PING, 0);
+//	Delay_ms(20);
+//
+//	SendMessageToModule(2,CODE_PING, 0);
+//	Delay_ms(20);
+//
+//	SendMessageToModule(4,CODE_PING, 0);
+//	Delay_ms(20);
+//
+//	SendMessageToModule(7,CODE_PING, 0);
+//	Delay_ms(20);
+
+}
 /*-----------------------------------------------------------*/
