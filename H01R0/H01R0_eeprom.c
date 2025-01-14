2636ba4417a0f66905180c65e3bb40aa2d7dd2fa
Abdulrhman_Battikh <abdulrhmanbattikh123@gmail.com>
2025-01-14 16:52:24 +0300

    Refactoring EEPROM files

--- H01R0/H01R0_eeprom.c
+++ H01R0/H01R0_eeprom.c
@@ -3,7 +3,7 @@
  All rights reserved
 
  File Name     : BOS_eeprom.c
- Description   : Source code for providing all the EEPROM emulation firmware functions.
+ Description   : EEPROM emulator library (Source file).
 
  */
 /* Includes ------------------------------------------------------------------*/
@@ -21,112 +21,107 @@ extern FLASH_ProcessTypeDef pFlash;
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 
-/**
+/******************************************************************************/
+
+/*
  * @brief  Restore the pages to a known good state in case of page's status
  *   corruption after a power loss.
  * @param  None.
- * @retval - Flash error code: on write Flash error
- *         - FLASH_COMPLETE: on success
+ * @retval - BOS_OK.
+ *         - BOS_ERR_EEPROM.
  */
-uint16_t EE_Init(void){
+BOS_Status EE_Init(void){
+	BOS_Status Status = BOS_OK;
 	HAL_FLASH_Unlock();
-	uint16_t InitStatus = EEPROM_Init(EE_FORCED_ERASE);
+	if(EE_OK != EEPROM_Init(EE_FORCED_ERASE))
+	{
+		HAL_FLASH_Lock();
+		return Status = BOS_ERR_EEPROM;
+	}
 	HAL_FLASH_Lock();
-	return InitStatus;
+	return Status;
 }
 
-/**
- * @brief  Returns the last stored variable data, if found, which correspond to
- *   the passed virtual address
- * @param  VirtAddress: Variable virtual address
- * @param  Data: Global variable contains the read variable value
- * @retval Success or error status:
- *           - EE_OK.
- *           - EE_INVALID_VIRTUALADDRESS.
- *           - EE_ERROR_NOACTIVE_PAGE.
+/******************************************************************************/
+
+/*
+ * @brief  Read the last stored variable data.
+ * @param  VirtAddress: Variable virtual address.
+ * @param  Data: Global variable contains the read variable value.
+ * @retval - BOS_OK.
+ *         - BOS_ERR_EEPROM.
  */
-uint16_t EE_ReadVariable(uint16_t VirtAddress,uint16_t *Data){
-
-	uint16_t ReadStatus = 0;
-	ReadStatus = EE_ReadVariable16bits(VirtAddress, Data);
-	if(EE_OK == ReadStatus)
-		return EE_OK;
-	else if(EE_INVALID_VIRTUALADDRESS == ReadStatus)
-		return EE_INVALID_VIRTUALADDRESS;
-	else if(EE_ERROR_NOACTIVE_PAGE == ReadStatus)
-		return EE_ERROR_NOACTIVE_PAGE;
-	else
-		return ReadStatus;
+BOS_Status EE_ReadVariable(uint16_t VirtAddress,uint16_t *Data){
+	BOS_Status Status = BOS_OK;
+	if(EE_OK != EE_ReadVariable16bits(VirtAddress, Data))
+		return Status = BOS_ERR_EEPROM;
+	return Status;
 }
 
-/**
+/******************************************************************************/
+
+/*
  * @brief  Writes/upadtes variable data in EEPROM.
- * @param  VirtAddress: Variable virtual address
- * @param  Data: 16 bit data to be written
- * @retval Success or error status:
- *           - EE_OK.
- *           - EE_INVALID_VIRTUALADDRESS.
- *           - EE_PAGE_FULL.
+ * @param  VirtAddress: Variable virtual address.
+ * @param  Data: 16 bit data to be written.
+ * @retval - BOS_OK.
+ *         - BOS_ERR_EEPROM.
  */
-uint16_t EE_WriteVariable(uint16_t VirtAddress,uint16_t Data){
+BOS_Status EE_WriteVariable(uint16_t VirtAddress,uint16_t Data){
 
-	uint16_t WriteStatus = 0;
+	BOS_Status Status = BOS_OK;
 	HAL_FLASH_Unlock();
-
-	WriteStatus = EE_WriteVariable16bits(VirtAddress, Data);
-	if(EE_OK == WriteStatus)
-		WriteStatus = EE_OK;
-	else if(EE_INVALID_VIRTUALADDRESS == WriteStatus)
-		WriteStatus = EE_INVALID_VIRTUALADDRESS;
-	else if(EE_CLEANUP_REQUIRED == WriteStatus)
-	{
-		EE_CleanUp();
-		WriteStatus = EE_CLEANUP_REQUIRED;
-	}
-
-	HAL_FLASH_Lock();
-	return WriteStatus;
-}
-
-/**
- * @brief  Erases PAGEA and PAGEB and writes VALID_PAGE header to PAGEA
- * @param  None
- * @retval Status of the last operation (Flash write or erase) done during
- *         EEPROM formating
- */
-uint16_t EE_Format(void){
-	HAL_StatusTypeDef FlashStatus =HAL_OK;
-	HAL_FLASH_Unlock();
-	if(EE_ERASE_ERROR != EEPROM_Format(EE_FORCED_ERASE))
+	if(EE_OK != EE_WriteVariable16bits(VirtAddress, Data))
 	{
 		HAL_FLASH_Lock();
-		return FlashStatus =HAL_OK;
+		return Status = BOS_ERR_EEPROM;
+	}
+	HAL_FLASH_Lock();
+	return Status;
+}
+
+/******************************************************************************/
+
+/*
+ * @brief  Erasing reserve pages for EEPROM emulator and assign the first page as Active.
+ * @param  None.
+ * @retval - BOS_OK.
+ *         - BOS_ERR_EEPROM.
+ */
+BOS_Status EE_Format(void){
+	BOS_Status Status = BOS_OK;
+	HAL_FLASH_Unlock();
+	if(EE_OK != EEPROM_Format(EE_FORCED_ERASE))
+	{
+		HAL_FLASH_Lock();
+		return Status = BOS_ERR_EEPROM;
 	}
 	else
 	{
 		HAL_FLASH_Lock();
-		return FlashStatus =HAL_ERROR;
+		return Status = BOS_ERR_EEPROM;
 	}
-
 }
 
-/**
+/******************************************************************************/
+
+/*
  * @brief  Writes/upadtes variable data in Flash.
- * @param  Address: Variable address
- * @param  Data: 16 bit data to be written
+ * @param  Address: Variable address.
+ * @param  Data: 16 bit data to be written.
  * @retval Success or error status:
- *           - FLASH_COMPLETE: on success
- *           - PAGE_FULL: if valid page is full
- *           - NO_VALID_PAGE: if no valid page was found
- *           - Flash error code: on write Flash error
+ *           - FLASH_COMPLETE: on success.
+ *           - PAGE_FULL: if valid page is full.
+ *           - NO_VALID_PAGE: if no valid page was found.
+ *           - Flash error code: on write Flash error.
  */
 uint16_t Flash_WriteVariable(uint32_t Address,uint16_t Data){
 	HAL_StatusTypeDef FlashStatus =HAL_OK;
-	
+
 	HAL_FLASH_Unlock();
-	
+
 	/* Set variable data */
-	//HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD,Address,Data);
+//	HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD,Address,Data);
 	//TOBECHECKED
 	/* Wait for last operation to be completed */
 	FlashStatus =FLASH_WaitForLastOperation((uint32_t )HAL_FLASH_TIMEOUT_VALUE);
@@ -145,28 +140,26 @@ uint16_t Flash_WriteVariable(uint32_t Address,uint16_t Data){
 	return FlashStatus;
 }
 
-/* Erase sector if force write is requested */
-BOS_Status EraseSector(uint32_t sector ) {
-	BOS_Status result = BOS_OK;
-	HAL_StatusTypeDef status;
+/******************************************************************************/
 
-		FLASH_EraseInitTypeDef erase;
-		uint32_t eraseError;
-		erase.TypeErase = FLASH_TYPEERASE_PAGES;
-		//erase.PageAddress =sector;
-		erase.Page =sector;
-		//TOBECHECKED
-		erase.NbPages =1;
-		status =HAL_FLASHEx_Erase(&erase,&eraseError);
-		if(status != HAL_OK || eraseError != 0xFFFFFFFF)
-			responseStatus =BOS_ERR_REMOTE_WRITE_FLASH;
-
-	return result;
+/*
+ * @brief  Erase sector in flash memory.
+ * @param  Sector : Sector to be erased.
+ * @retval - BOS_OK.
+ *         - BOS_ERR_REMOTE_WRITE_FLASH.
+ */
+BOS_Status EraseSector(uint32_t Sector ) {
+	BOS_Status Status = BOS_OK;
+	FLASH_EraseInitTypeDef erase;
+	uint32_t eraseError;
+	erase.TypeErase = FLASH_TYPEERASE_PAGES;
+	//erase.PageAddress =sector;
+	erase.Page = Sector;
+	//TOBECHECKED
+	erase.NbPages =1;
+	if((HAL_OK != HAL_FLASHEx_Erase(&erase,&eraseError)) || eraseError != 0xFFFFFFFF)
+		responseStatus = BOS_ERR_REMOTE_WRITE_FLASH;
+	return Status;
 }
 
-
-/**
- * @}
- */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+/************************ (C) COPYRIGHT HEXABITZ *****END OF FILE****/
--- H01R0/H01R0_eeprom.h
+++ H01R0/H01R0_eeprom.h
@@ -1,33 +1,49 @@
-/*
+ /*
  BitzOS (BOS) V0.3.6 - Copyright (C) 2017-2024 Hexabitz
  All rights reserved
 
  File Name     : BOS_eeprom.h
- Description   : Header file contains all the functions prototypes for the EEPROM
- 	 	 	 	 emulation firmware library.
+ Description   : EEPROM emulator library (Header file).
 
+ * EEPROM Emulation Description:
+ * -----------------------------
+ * - The EEPROM emulation is developed to store 1000 elements, where each element is 8 bytes (64 bits).
+ * - The total storage requires 4 pages of flash memory, calculated as follows:
+ *     Number of pages = (Number of elements × 8) / Flash page size
+ *                    = (1000 × 8) / 2048
+ *                    = 4 pages
+ *
+ * Memory Organization:
+ * -------------------
+ * - Element Structure (8 Bytes / 64 Bits):
+ *   - Bits 63–32: Data Value (32 bits)
+ *   - Bits 31–16: CRC (16 bits)
+ *   - Bits 15–0:  Virtual Address (16 bits)
+ *
+ * - Page Allocation:
+ *   - First Group:  4 pages reserved for storing EEPROM elements.
+ *   - Second Group: 4 additional pages reserved as a backup.
+ *
+ * Storage Mechanism:
+ * -----------------
+ * 1. Initial Storage:
+ *    - Data is stored in the first group of 4 pages.
+ *
+ *2. Group Swap:
+ *    - If the first group becomes full, the latest changes are transferred from the first group to the second group.
+ *    - The storage process is then completed in the second group.
+ *    - If the second group becomes full, the latest changes are transferred from the second group to the first group.
+ *    - The storage process is then completed in the first group.
+ *
+ * 3. Cycle Continuation:
+ *    - This process alternates between the two groups to ensure efficient and reliable storage.
+ *
+ * Purpose of the Second Group:
+ * ---------------------------
+ * - The second group acts as a backup to facilitate the transfer of updated elements between the two groups.
+ * - This ensures that the latest data is always preserved and the storage process is seamless.
+ *
  */
-
-/*
- * EEPROM Emulated Description :
- * number of EEPROM elements is 1000 which can be stored in EEPROM emulated in 4 pages
- * where Each element is 8 bytes :
- *  Description of the 8 Bytes (64 bits) element in flash
- *  Bit:  63                  32  31      16  15         0
- *       <--- Data Value ----->  <-unused->  <-VirtAddr->
- *  number of pages = (number of elements * 8) / flash page size
- *  				= (1000 * 8) / 2048
- *  				= 4 pages
- *  We will reserve 4 pages as a first group + 4 pages backup as a second group
- *  The purpose of the second group is to transfer the latest update of the elements
- *  from the first group to the second group, then complete the storage process in
- *  the second group.If the second group is full, the latest updates of the elements will
- *  be transferred from the second group to the first group, and then the storage process
- *  will be completed in the first group.And so on.
- *
- *
- *
- * */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __BOS_EEPROM_H
@@ -38,49 +54,48 @@
 
 /* Exported constants --------------------------------------------------------*/
 
-/* EEPROM Variables' number (up to 1024 16-bit variables) */
-#define NumOfEEPROMvar        1000
+/* EEPROM Variables' number (up to 1000 16-bit variables) */
+#define NumOfEEPROMvar              1000
 
 /* EEPROM virtual addresses - Consider MaxNumOfModules is 25 */
-// BOS Addressing Space 1 - 499
-#define _EE_NBASE							1
-#define _EE_PORT_DIR_BASE					2				// Todo: Move to RO - 25 modules - 25 variables
-#define _EE_ALIAS_BASE						28				// 25 modules/10 chars - 125 variables
-#define _EE_GROUP_ALIAS_BASE			    153				// 10 groups/10 chars - 50 variables
-#define _EE_GROUP_MODULES_BASE		   		203				// 25 modules - 25 variables
-#define _EE_DMA_STREAM_BASE					228				// 8 variables
-#define _EE_BUTTON_BASE						236				// 4 * MaxNumOfPorts (10) variables for buttons: port(4 bits), type (4 bits), events (8 bits)
-															// pressed_for_x_1 (8 bits), released_for_y_1 (8 bits), etc.
-#define _EE_PARAMS_BASE						276				// Parameter base: BOS trace (MSB) | BOS response - 1 variable 
-#define _EE_PARAMS_DEBOUNCE					277				// Parameter: Button debounce - 1 variable
-#define _EE_PARAMS_SINGLE_CLICK				278				// Parameter: Button single click - 1 variable
-#define _EE_PARAMS_DBL_CLICK				279				// Parameter: Button double click (inter-click min and max) - 1 variable
-#define _EE_CLI_BAUD						280				// Parameter: CLI baudrate - LSB halfword, MSB halfword - 2 variables
-#define _EE_PARAMS_RTC						282				// Parameter: RTC hourformat | RTC daylightsaving - 1 variable
-#define _EE_PARAMS_DISABLE_CLI				283				// Parameter: Disable CLI - 1 variable
-#define _EE_PARAMS_Messaging			    284				// Parameter base: BOSMessaging Acknowledgment (MSB) | BOS trial - 1 variable
 
-// Module Addressing Space 500 - 599
-#define _EE_MODULE_BASE						500
+/* BOS Addressing Space 1 - 499 */
+#define _EE_NBASE					1
+#define _EE_PORT_DIR_BASE			2		/* 25 modules - 25 variables */
+#define _EE_ALIAS_BASE				28		/* 25 modules/10 chars - 125 variables */
+#define _EE_GROUP_ALIAS_BASE		153		/* 10 groups/10 chars - 50 variables */
+#define _EE_GROUP_MODULES_BASE		203		/* 25 modules - 25 variables */
+#define _EE_DMA_STREAM_BASE			228		/* 8 variables */
+#define _EE_BUTTON_BASE				236		/* 4 * MaxNumOfPorts (10) variables for buttons: port(4 bits), type (4 bits), events (8 bits)
+											 * pressed_for_x_1 (8 bits), released_for_y_1 (8 bits), etc. */
+#define _EE_PARAMS_BASE				276		/* Parameter base: BOS trace (MSB) | BOS response - 1 variable */
+#define _EE_PARAMS_DEBOUNCE			277		/* Parameter: Button debounce - 1 variable */
+#define _EE_PARAMS_SINGLE_CLICK		278		/* Parameter: Button single click - 1 variable */
+#define _EE_PARAMS_DBL_CLICK		279		/* Parameter: Button double click (inter-click min and max) - 1 variable */
+#define _EE_CLI_BAUD				280		/* Parameter: CLI baudrate - LSB halfword, MSB halfword - 2 variables */
+#define _EE_PARAMS_RTC				282		/* Parameter: RTC hourformat | RTC daylightsaving - 1 variable */
+#define _EE_PARAMS_DISABLE_CLI		283		/* Parameter: Disable CLI - 1 variable */
+#define _EE_PARAMS_Messaging	    284		/* Parameter base: BOSMessaging Acknowledgment (MSB) | BOS trial - 1 variable */
 
-// User Addressing Space 600 - 1024
-#define _EE_EMPTY_VAR_BASE					600
+/* Module Addressing Space 500 - 599 */
+#define _EE_MODULE_BASE				500
+
+/* User Addressing Space 600 - 1000 */
+#define _EE_EMPTY_VAR_BASE			600
 
 #if MaxNumOfModules > 26						// Update
  #warning "Data for 26 modules only will be stored in EEPROM."
 #endif
 
-/* Exported variables --------------------------------------------------------*/
 
-/* Exported types ------------------------------------------------------------*/
-/* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
-uint16_t EE_Init(void);
-uint16_t EE_Format(void);
-uint16_t EE_ReadVariable(uint16_t VirtAddress,uint16_t *Data);
-uint16_t EE_WriteVariable(uint16_t VirtAddress,uint16_t Data);
+BOS_Status EE_Init(void);
+BOS_Status EE_Format(void);
+BOS_Status EE_ReadVariable(uint16_t VirtAddress,uint16_t *Data);
+BOS_Status EE_WriteVariable(uint16_t VirtAddress,uint16_t Data);
 uint16_t Flash_WriteVariable(uint32_t Address,uint16_t Data);
-BOS_Status EraseSector(uint32_t sector );
+BOS_Status EraseSector(uint32_t Sector);
+
 #endif /* __BOS_EEPROM_H */
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+/************************ (C) COPYRIGHT HEXABITZ *****END OF FILE****/
