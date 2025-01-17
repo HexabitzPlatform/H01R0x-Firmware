8d291f90f7daa1cfa68b38693af45537f488f737
Abdulrhman_Battikh <abdulrhmanbattikh123@gmail.com>
2025-01-17 22:57:17 +0300

    Editing on EEPROM APIs

--- H01R0/H01R0_eeprom.c
+++ H01R0/H01R0_eeprom.c
@@ -8,17 +8,10 @@
  */
 /* Includes ------------------------------------------------------------------*/
 #include "BOS.h"
-#include "H01R0_eeprom.h"
-#include "eeprom_emul.h"
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
 
 /* Variables used for Erase pages under interruption */
 extern FLASH_ProcessTypeDef pFlash;
 
-/* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 
 /******************************************************************************/
@@ -32,13 +25,18 @@ extern FLASH_ProcessTypeDef pFlash;
  */
 BOS_Status EE_Init(void){
 	BOS_Status Status = BOS_OK;
+	EE_Status eeStatus = EE_OK;
+
 	HAL_FLASH_Unlock();
-	if(EE_OK != EEPROM_Init(EE_FORCED_ERASE))
+	eeStatus = EEPROM_Init(EE_FORCED_ERASE);
+	if((EE_WRITE_ERROR == eeStatus || EE_INVALID_PAGE_SEQUENCE == eeStatus || EE_TRANSFER_ERROR == eeStatus))
 	{
+		EE_Format();
 		HAL_FLASH_Lock();
-		return Status = BOS_ERR_EEPROM;
+		NVIC_SystemReset();
 	}
 	HAL_FLASH_Lock();
+
 	return Status;
 }
 
@@ -53,8 +51,10 @@ BOS_Status EE_Init(void){
  */
 BOS_Status EE_ReadVariable(uint16_t VirtAddress,uint16_t *Data){
 	BOS_Status Status = BOS_OK;
+
 	if(EE_OK != EE_ReadVariable16bits(VirtAddress, Data))
 		return Status = BOS_ERR_EEPROM;
+
 	return Status;
 }
 
@@ -68,15 +68,22 @@ BOS_Status EE_ReadVariable(uint16_t VirtAddress,uint16_t *Data){
  *         - BOS_ERR_EEPROM.
  */
 BOS_Status EE_WriteVariable(uint16_t VirtAddress,uint16_t Data){
-
 	BOS_Status Status = BOS_OK;
+	EE_Status eeStatus = EE_OK;
+
 	HAL_FLASH_Unlock();
-	if(EE_OK != EE_WriteVariable16bits(VirtAddress, Data))
+	eeStatus = EE_WriteVariable16bits(VirtAddress, Data);
+	if (EE_CLEANUP_REQUIRED == eeStatus)
+	{
+		EE_CleanUp();
+	}
+	else if(EE_OK != eeStatus)
 	{
 		HAL_FLASH_Lock();
 		return Status = BOS_ERR_EEPROM;
 	}
 	HAL_FLASH_Lock();
+
 	return Status;
 }
 
@@ -90,17 +97,17 @@ BOS_Status EE_WriteVariable(uint16_t VirtAddress,uint16_t Data){
  */
 BOS_Status EE_Format(void){
 	BOS_Status Status = BOS_OK;
+
 	HAL_FLASH_Unlock();
 	if(EE_OK != EEPROM_Format(EE_FORCED_ERASE))
 	{
 		HAL_FLASH_Lock();
 		return Status = BOS_ERR_EEPROM;
 	}
-	else
-	{
-		HAL_FLASH_Lock();
-		return Status = BOS_ERR_EEPROM;
-	}
+	HAL_FLASH_Lock();
+
+	return Status;
+
 }
 
 /******************************************************************************/
@@ -119,7 +126,6 @@ uint16_t Flash_WriteVariable(uint32_t Address,uint16_t Data){
 	HAL_StatusTypeDef FlashStatus =HAL_OK;
 
 	HAL_FLASH_Unlock();
-
 	/* Set variable data */
 //	HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD,Address,Data);
 	//TOBECHECKED
@@ -133,7 +139,6 @@ uint16_t Flash_WriteVariable(uint32_t Address,uint16_t Data){
 		/* If the program operation is completed, disable the PG Bit */
 		CLEAR_BIT(FLASH->CR,FLASH_CR_PG);
 	}
-	
 	HAL_FLASH_Lock();
 	
 	/* Return last operation flash status */
@@ -152,6 +157,7 @@ BOS_Status EraseSector(uint32_t Sector ) {
 	BOS_Status Status = BOS_OK;
 	FLASH_EraseInitTypeDef erase;
 	uint32_t eraseError;
+
 	erase.TypeErase = FLASH_TYPEERASE_PAGES;
 	//erase.PageAddress =sector;
 	erase.Page = Sector;
@@ -159,6 +165,7 @@ BOS_Status EraseSector(uint32_t Sector ) {
 	erase.NbPages =1;
 	if((HAL_OK != HAL_FLASHEx_Erase(&erase,&eraseError)) || eraseError != 0xFFFFFFFF)
 		responseStatus = BOS_ERR_REMOTE_WRITE_FLASH;
+
 	return Status;
 }
 
