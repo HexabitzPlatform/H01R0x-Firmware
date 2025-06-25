3dedd59a477af47eebbb5c93a1e5fe9424795c9b
Bakry Rihawi <bakry29bbb@gmail.com>
2025-06-25 16:43:19 +0300

    Adding ADC function updates for both APIs and CLI.

--- User/main.c
+++ User/main.c
@@ -10,7 +10,14 @@
 #include "BOS.h"
 
 /* Private variables *******************************************************/
-
+float adcValue1 = 0;
+float adcValue2 = 0;
+float adcValue3 = 0;
+float adcValue4 = 0;
+float percentageAdcValue1 = 0;
+float percentageAdcValue2 = 0;
+float percentageAdcValue3 = 0;
+float percentageAdcValue4 = 0;
 /* Private Function Prototypes *********************************************/
 
 /* Main Function ***********************************************************/
@@ -27,11 +34,21 @@ int main(void){
 /***************************************************************************/
 /* User Task */
 void UserTask(void *argument){
-
+	 ADCSelectPort(P1);
+	 ADCSelectPort(P2);
 	/* put your code here, to run repeatedly. */
 	while(1){
+			ReadADCChannel(P1, TOP, &adcValue1);
+			ReadADCChannel(P1, BOTTOM, &adcValue2);
+			ReadADCChannel(P2, TOP, &adcValue3);
+			ReadADCChannel(P2, BOTTOM, &adcValue4);
 
-	}
+			GetReadPercentage(P1, TOP, &percentageAdcValue1);
+			GetReadPercentage(P1, BOTTOM, &percentageAdcValue2);
+			GetReadPercentage(P2, TOP, &percentageAdcValue3);
+			GetReadPercentage(P2, BOTTOM, &percentageAdcValue4);
+
+}
 }
 
 /***************************************************************************/
